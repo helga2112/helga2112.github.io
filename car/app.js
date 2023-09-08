ede22292e0487002e78033625b3238bc2e057478
Olga <46113038+helga2112@users.noreply.github.com>
2023-09-08 11:43:20 +0100

    preplay added

--- car/app.js
+++ car/app.js
@@ -1,38 +1,65 @@
-const intro = document.querySelector('.intro')
-const video = intro.querySelector('video')
-const text = intro.querySelector('h1')
+function isAllpeDevice() {
+  this.toMatch = [/iPhone/i, /iPad/i, /iPod/i];
+  // return this.toMatch.some(toMatchItem => navigator.userAgent.match(toMatchItem));
+  return true;
+}
 
-const section = document.querySelector('section')
-const end = section.querySelector('h1')
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
 
-const controller = new ScrollMagic.Controller()
+async function playVideo() {
+  video.setAttribute('autoplay', true)
+  setTimeout(() => {
+    video.pause()
+  }, 700);
+}
+
+
+playVideo()
+
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+
+const controller = new ScrollMagic.Controller();
+
+console.log("is Apple device:", isAllpeDevice());
+
+/* window.onload = function () {
+  //  autoplay loop muted playsinline
+  if (isAllpeDevice) {
+    document.querySelector("video").load();
+    document.querySelector("video").play();
+    setTimeout(() => {
+      // video.pause();
+    }, 20000);
+  }
+};
+ */
 
 // scenes
 const scene = new ScrollMagic.Scene({
-    duration: 4000,
-    triggerElement: intro,
-    triggerHook: 0
+  duration: 4000,
+  triggerElement: intro,
+  triggerHook: 0,
 })
-.addIndicators()
-.setPin(intro)
-.addTo(controller)
-
+  .addIndicators()
+  .setPin(intro)
+  .addTo(controller);
 // video animation
-let accelAmount = 0.1  // like easing
-let scrollPosition = 0
-let delay = 0
+let accelAmount = 0.1; // like easing
+let scrollPosition = 0;
+let delay = 0;
 
 console.log(video.currentTime);
-console.log(video.preload)
+console.log(video.preload);
 
+scene.on("update", (e) => {
+  scrollPosition = e.scrollPos / 1500;
+  video.currentTime = scrollPosition;
+});
 
 /* scene.on('update', e => {
-    scrollPosition = e.scrollPos / 1500
-    video.currentTime = scrollPosition
-}) */
-
-
-scene.on('update', e => {
     scrollPosition = e.scrollPos / 1000
 })
 
@@ -41,3 +68,4 @@ setInterval(()=>{
     video.currentTime = delay
     console.log("delay", delay.toFixed(2), "scrollPosition",scrollPosition.toFixed(2))
 }, 33)
+ */
