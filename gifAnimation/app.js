a2dfb54b918c295232e7a926f7a463d6f6f066c1
Olga <46113038+helga2112@users.noreply.github.com>
2023-09-11 15:42:18 +0100

    gif added

---
+++ gifAnimation/app.js
@@ -0,0 +1,61 @@
+/* var gifFrames = require ('gif-frames');*/
+/* var fs = require('fs'); */
+
+/* import gifFrames from 'gif-frames'
+import fs from 'fs' */
+
+console.log(p5)
+
+const intro = document.querySelector('.intro')
+const car = intro.querySelector('img')
+this.videoSource = car.querySelector('source')
+this.mobileSource = this.videoSource.getAttribute('mobile_video') 
+this.videoSource.setAttribute('src', this.mobileSource);
+const text = intro.querySelector('h1')
+
+const section = document.querySelector('section')
+const end = section.querySelector('h1')
+
+const controller = new ScrollMagic.Controller()
+
+// scenes
+const scene = new ScrollMagic.Scene({
+    duration: 3400,
+    triggerElement: intro,
+    triggerHook: 0
+})
+.addIndicators()
+.setPin(intro)
+.addTo(controller)
+
+// car animation
+let accelAmount = 0.2  // like easing
+let scrollPosition = 0
+let delay = 0
+
+console.log('duration', car.duration);
+console.log('mediakeys',car.mediaKeys)
+console.log('playbackRate',car.playbackRate)
+console.log('totalVideoFrames',car.getVideoPlaybackQuality())
+
+var rounded = function(number){
+    return +number.toFixed(2);
+}
+
+/* scene.on('update', e => {
+    scrollPosition = e.scrollPos / 1000
+    car.currentTime = scrollPosition
+}) */
+
+console.log('>>> car', car.duration)
+
+/* setInterval(()=>{
+    console.log('>>> car', car.duration)
+    delay += (scrollPosition - delay) * accelAmount
+    car.currentTime = delay
+}, 33.3) */
+
+
+// 1 sec = 1000 millisec 
+// car framerate = 30
+// 1000 / 30 = 33.3
